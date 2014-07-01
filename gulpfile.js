1da2902a6b599378a475f4fc4b28c505c1017458
Al Nemec <al.nemec@gmail.com>
2014-07-01 07:10:25 -0500

    Updated the gulp file to work with the folder name changes.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var gulp = require('gulp'),
 
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('css/*.scss')
+  return gulp.src('sass/*.scss')
     .pipe(sass({ style: 'expanded', }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/styles'))
@@ -81,7 +81,7 @@ gulp.task('watch', function() {
     };
 
     // Watch .scss files
-    gulp.watch('scss/**/*.scss', ['styles']);
+    gulp.watch('sass/**/*.scss', ['styles']);
 
     // Watch .js files
     gulp.watch('js/**/*.js', ['scripts']);
